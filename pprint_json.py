d2671c5e85a1df0497005abe53fcff4c78800d18
cr00z <netrebinr@gmail.com>
2019-04-01 23:03:43 +0700

    make third attempt

--- pprint_json.py
+++ pprint_json.py
@@ -14,7 +14,7 @@ def gen_indent(indent_level):
     return '    ' * indent_level
 
 
-def gen_print_json_lst(json_data, open_char, close_char, indent_level):
+def gen_print_json_array(json_data, open_char, close_char, indent_level):
     indent = gen_indent(indent_level)
     last_indent = gen_indent(indent_level - 1)
     out_list = []
@@ -24,32 +24,49 @@ def gen_print_json_lst(json_data, open_char, close_char, indent_level):
     return open_char + ','.join(out_list) + '\n' + last_indent + close_char
 
 
+def pretty_print_json_list(json_data, indent_level):
+    return gen_print_json_array(json_data, '[', ']', indent_level)
+
+
+def pretty_print_json_dict(json_data, indent_level):
+    return gen_print_json_array(json_data.items(), '{', '}', indent_level)
+
+
+def pretty_print_json_tuple(json_data, indent_level):
+    key, value = json_data
+    return '"' + key + '": ' + pretty_print_json(value, indent_level)
+
+
+def pretty_print_json_number(json_data, indent_level):
+    return str(json_data)
+
+
+def pretty_print_json_string(json_data, indent_level):
+    return '"' + json_data + '"'
+
+
+def pretty_print_json_bool(json_data, indent_level):
+    # a little bit of a bollywood magic
+    return str(json_data).lower()
+
+
+def pretty_print_json_null(json_data, indent_level):
+    return 'null'
+
+
+# имхо вариант с ифами поудачнее :( но бот не пропустил
+# https://github.com/cr00z/4_json/blob/7ebe4254b901e66fb3eb18f07ebbe8dbb08fadd4/pprint_json.py
 def pretty_print_json(json_data, indent_level=1):
-    if type(json_data) is list:
-        return gen_print_json_lst(iter(json_data), '[', ']', indent_level)
-
-    if type(json_data) is dict:
-        return gen_print_json_lst(json_data.items(), '{', '}', indent_level)
-
-    if type(json_data) is tuple:
-        key, value = json_data
-        return '"' + key + '": ' + pretty_print_json(value, indent_level)
-
-    if type(json_data) is int or \
-       type(json_data) is float:
-        return str(json_data)
-
-    if type(json_data) is str:
-        return '"' + json_data + '"'
-
-    if type(json_data) is bool:
-        # a little bit of a bollywood
-        return str(json_data).lower()
-
-    if json_data is None:
-        return 'null'
-
-    return ''
+    return {
+        type(json_data) is list: pretty_print_json_list,
+        type(json_data) is dict: pretty_print_json_dict,
+        type(json_data) is tuple: pretty_print_json_tuple,
+        type(json_data) is int or
+        type(json_data) is float: pretty_print_json_number,
+        type(json_data) is str: pretty_print_json_string,
+        type(json_data) is bool: pretty_print_json_bool,
+        json_data is None: pretty_print_json_null
+    }[True](json_data, indent_level)
 
 
 if __name__ == '__main__':
