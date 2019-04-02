a89b14ee4c2d57e199f54337cc2398fded380f12
cr00z <netrebinr@gmail.com>
2019-04-02 21:34:41 +0700

    make fifth attempt

--- pprint_json.py
+++ pprint_json.py
@@ -11,20 +11,19 @@ def load_data(filepath):
 
 
 def get_cmdline_args():
-    # не понял замечание, зачем использовать парсер второй раз?
     parser = argparse.ArgumentParser(description='JSON output in pretty form')
     parser.add_argument('json_filename', metavar='json_filename', type=str,
                         help='JSON filename')
     return parser.parse_args()
 
 
-def pretty_print_json(raw_object):
-    return json.dumps(raw_object, ensure_ascii=False, indent=4)
+def pretty_print_json(python_object):
+    return json.dumps(python_object, ensure_ascii=False, indent=4)
 
 
 if __name__ == '__main__':
     args = get_cmdline_args()
-    raw_object = load_data(args.json_filename)
-    if raw_object is None:
+    python_object = load_data(args.json_filename)
+    if python_object is None:
         exit('Input file not found or not a JSON')
-    print(pretty_print_json(raw_object))
+    print(pretty_print_json(python_object))
